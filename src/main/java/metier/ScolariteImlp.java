b4651e9b5c8d43d2e362b021cbe2057b8b85b31c
Imedbennington <bouzidichaz@gmail.com>
2024-05-02 16:17:48 +0200

    msg

--- src/main/java/metier/ScolariteImlp.java
+++ src/main/java/metier/ScolariteImlp.java
@@ -2,10 +2,7 @@ package metier;
 
 import dao.SingletonConnection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,12 +13,68 @@ private Connection connection;
     }
     @Override
     public void addEtudiant(Etudiant et) {
+        /*try {
 
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String sql = "insert into etudiant (name, familyname, mail, estate) values(?, ?, ?, ?)";
+            PreparedStatement ps =connection.prepareStatement(sql);
+            ps.setString(1, et.getName());
+            ps.setString(2, et.getFamilyName());
+            ps.setString(3, et.getMail());
+            ps.setString(4, et.getState());
+            ps.executeUpdate();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }*/
+         /* Communication avec la base
+         */
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+//            Connection connection = DriverManager.getConnection
+//                    (url, user, pass);
+
+            String sql = "insert into etudiant (name, familyname, mail, estate) values(?,?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, et.getName());
+            ps.setString(2, et.getFamilyName());
+            ps.setString(3, et.getMail());
+            ps.setString(4, et.getState());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Etudiant> listEtudiants() {
-        return null;
+
+        List<Etudiant> etudiants = new ArrayList<>();
+        /*
+         * Communication avec la base
+         */
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String sql = "select * from etudiant";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Etudiant etudiant = new Etudiant();
+                etudiant.setIdEtudiant(rs.getLong("id"));
+                etudiant.setName(rs.getString("name"));
+                etudiant.setFamilyName(rs.getString("familyname"));
+                etudiant.setMail(rs.getString("mail"));
+                etudiant.setState((rs.getString("estate")));
+                etudiants.add(etudiant);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return etudiants;
     }
 
     @Override
@@ -61,7 +114,32 @@ private Connection connection;
 
     @Override
     public Etudiant getEtudiant(Long id) {
-        return null;
+        List<Etudiant> etudiants = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String sql = "select * from etudiant where id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Etudiant etudiant = new Etudiant();
+                etudiant.setIdEtudiant(rs.getLong("id"));
+                etudiant.setName(rs.getString("name"));
+                etudiant.setFamilyName(rs.getString("familyname"));
+                etudiant.setMail(rs.getString("mail"));
+                etudiant.setState((rs.getString("estate")));
+                etudiants.add(etudiant);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if (!etudiants.isEmpty()) {
+            return etudiants.get(0);
+        } else {
+            // Return null or handle appropriately when no student is found
+            return null;
+        }
     }
 
     @Override
--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -4,26 +4,69 @@ import dao.SingletonConnection;
 import metier.Etudiant;
 import metier.ScolariteImlp;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //SingletonConnection.afficher();
-        Etudiant et = new Etudiant();
+        boolean create = false;
+        ScolariteImlp scl = new ScolariteImlp();
         Scanner scanner = new Scanner(System.in);
+        //SingletonConnection.afficher();
+
         ScolariteImlp metier = new ScolariteImlp();
         System.out.println("Par nom oui ou nn");
-        if (scanner.nextLine().equalsIgnoreCase("o")){
+        if (scanner.nextLine().equalsIgnoreCase("o")) {
             System.out.println("Mot cle");
             String motcle = scanner.nextLine();
             List<Etudiant> etudiantList = metier.etudiantsParMC(motcle);
-            if (!etudiantList.isEmpty()){
+            /*if (!etudiantList.isEmpty()){
                 for (Etudiant etudiant:etudiantList){
                     System.out.println(etudiant.getName());
                 }
             }
         }
-    }
+            System.out.println("************;Ajout etudiant ****************");
+            System.out.println("name");
+            String name = scanner.nextLine();
+            System.out.println("familyname");
+            String fname = scanner.nextLine();
+            System.out.println("mail");
+            String mail = scanner.nextLine();
+            System.out.println("estate");
+            String estate = scanner.nextLine();
+            Etudiant et = new Etudiant(name, fname, mail, estate);
+            scl.addEtudiant(et);
 
+            System.out.println(et.getName());
+            System.out.println(et.getFamilyName());
+            System.out.println(et.getMail());
+            System.out.println(et.getState());*/
+
+            /*
+            // Call the listEtudiants() method to retrieve the list of students
+            List<Etudiant> etudiants = scl.listEtudiants();
+
+            // Process the retrieved list of students (e.g., print them)
+            for (Etudiant etudiant : etudiants) {
+                System.out.println(etudiant);
+            }*/
+            System.out.println("donner id ");
+            Integer id = Integer.valueOf(scanner.nextLine());
+            Etudiant etudiant = new Etudiant();
+            etudiant = scl.getEtudiant(Long.valueOf(id));
+            if (etudiant != null) {
+                // Print the details of the retrieved student
+                System.out.println("Student found:");
+                System.out.println("ID: " + etudiant.getIdEtudiant());
+                System.out.println("Name: " + etudiant.getName());
+                System.out.println("Family Name: " + etudiant.getFamilyName());
+                System.out.println("Mail: " + etudiant.getMail());
+                System.out.println("State: " + etudiant.getState());
+            } else {
+                System.out.println("No student found with the provided ID.");
+            }
+        }
+    }
 }
